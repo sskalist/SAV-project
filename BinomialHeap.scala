174db045486e2e494bf9d554b820761e90b105aa
sskalist <stefanos.skalistis@epfl.ch>
2015-06-06 19:31:28 +0200

    Updates

--- BinomialHeap.scala
+++ BinomialHeap.scala
@@ -5,11 +5,14 @@ import leon.annotation._
 
 
 object BinomialTree{
+
+
+/* ====================   BinomialTree   ========================= */
   
   case class BinomialTree(forest: BinomialForest, value: BigInt) { 
     
     def rank: BigInt = {
-      this.forest.rank + 1
+      this.forest.size
     }
     
     def contents: Set[BigInt] = {
@@ -17,34 +20,65 @@ object BinomialTree{
     }
     
     def mergeEqual(that: BinomialTree): BinomialTree = {
-      require( this.rank == that.rank && equalRank(this,that) )
+      require( this.isBT && that.isBT && this.rank == that.rank && 
+               equalRankLemma(this,that) )
         if (this.value > that.value) 
           BinomialTree(that.forest ++ this, that.value) 
         else 
           BinomialTree(this.forest ++ that, this.value)
     } ensuring {
-      res => res.contents == this.contents ++ that.contents
+      res => res.contents == this.contents ++ that.contents && res.isBT
     }
     
+    def isBT: Boolean = {
+      this.forest.isFull && 
+      this.rank == this.forest.maxRank + 1 
+    }
   }
   
-  def equalRank(a :BinomialTree, b: BinomialTree) = {
-    require( a.rank == b.rank)
-    (a.rank >= 0) && (b.rank >= 0) && 
-    (a.rank == a.forest.rank + 1) && 
-    (b.rank == b.forest.rank + 1) &&
-    (a.forest.rank == b.forest.rank) && 
+  def equalRankLemma(a :BinomialTree, b: BinomialTree) = {
+    require(a.isBT && b.isBT && a.rank == b.rank ) // && a.forest.isFull && b.forest.isFull)
+    treeLemma(a) && treeLemma(b) && 
+    (a.forest.size == b.forest.size) && 
     (a.rank == b.rank)
   }.holds
   
+  def treeLemma(a :BinomialTree) = {
+    require(a.isBT)
+    (a.rank >= 0) && 
+    (a.rank == a.forest.size) &&
+    a.forest.isOrdered
+  }.holds
+
+  
+
+/* ====================   BinomialForest   ========================= */
+
   abstract class BinomialForest {
     
-    def rank: BigInt = {
+    def minRank: BigInt = {
+      require(this.isOrdered)
       this match {
         case Nil() => -1
-        case Cons(h,t) => t.rank + 1
+        case Cons(h,t) => h.rank
       }
     } ensuring { res => (this.isEmpty && res == -1) || (!this.isEmpty && res >= 0) }
+    
+    def maxRank: BigInt = {
+      require(this.isOrdered)
+      this match {
+        case Nil() => -1
+        case Cons(h,Nil()) => h.rank
+        case Cons(_,t) => t.maxRank
+      }
+    } ensuring { res => (!this.isEmpty && res >= 0) || (this.isEmpty && res == -1) }
+    
+    def size: BigInt = {
+      this match {
+        case Nil() => 0
+        case Cons(h,t) => t.size + 1
+      }
+    } ensuring { _ >= 0 }
     
     def isEmpty: Boolean = {
       this match {
@@ -60,62 +94,103 @@ object BinomialTree{
       }
     }
     
+    // Adding a tree with a rank exactly one higher
+    //@induct
     def ++(tree: BinomialTree): BinomialForest = {
-      require(this.rank + 1 == tree.rank)
-      Cons(tree,this)
+      require(tree.isBT && this.isFull && ( (this.isEmpty && tree.rank == 0) || (!this.isEmpty && tree.rank == this.maxRank + 1)) )
+      this match {
+        case Nil() => Cons(tree,Nil())
+	      case _ => this.addHelper(tree)
+      }
     } ensuring {
-      res => res.rank == tree.rank && res.contents == this.contents ++ tree.contents
+      res => res.size == this.size + 1 && res.contents == this.contents ++ tree.contents && res.isFull
     }
     
+    private def addHelper(tree: BinomialTree): BinomialForest = {
+      require(tree.isBT && this.isOrdered && (this.isEmpty || tree.rank == this.maxRank + 1) )
+      this match {
+        case Nil() => Cons(tree,Nil())
+	      case Cons(h,t) => Cons(h,t.addHelper(tree))
+      }
+    } ensuring {
+      res => res.size == this.size + 1 && res.contents == this.contents ++ tree.contents
+    }
+    
+    def isOrdered: Boolean = {
+      this match {
+        case Nil() => true
+        case Cons(h,Nil()) => true
+        case Cons(h,f @ Cons(h2,t) ) => if( h.rank < h2.rank ) f.isOrdered else false
+      }
+    } 
+    
+    def isCompact: Boolean = {
+      this match {
+        case Nil() => true
+        case Cons(h,Nil()) => h.rank == this.maxRank
+        case Cons(h, t @ Cons(hh,_) ) => h.rank == hh.rank + 1 && t.isCompact
+      }
+    }
+    
+    def isFull: Boolean = {
+      this match {
+        case Nil() => true
+        case Cons(h, t ) => h.rank == 0 && isFullHelper(h.rank,t)
+      }
+    }
+    
+    def isFullHelper(rank:BigInt, tail: BinomialForest): Boolean = {
+      tail match{
+        case Nil() => true
+        case Cons(h,t) => h.rank == rank + 1 && isFullHelper(h.rank,t)
+      } 
+    }
   }
-  case class Nil() extends BinomialForest
-  case class Cons(head: BinomialTree, tail: BinomialForest) extends BinomialForest
+
+  private case class Nil() extends BinomialForest
+  private case class Cons(head: BinomialTree, tail: BinomialForest) extends BinomialForest
+
+   
+
+/* ====================   BinomialHeap   ========================= */
   
-  case class BinomialHeap(forest: BinomialForest) {
-      def isEmpty: Boolean = {
-        forest.isEmpty
-      }
+  // case class BinomialHeap(forest: BinomialForest) {
+  //     def isEmpty: Boolean = {
+  //       forest.isEmpty
+  //     }
       
       
       
-      def +(value: BigInt) : BinomialHeap = {
-        this//BinomialTree(Nil(),value)
-      }
-  }
+  //     def +(value: BigInt) : BinomialHeap = {
+  //       this//BinomialTree(Nil(),value)
+  //     }
+  // }
  
-  def insertTree(tree: BinomialTree, forest: BinomialForest) : BinomialForest = {
-    require( (tree.rank <= forest.rank) || forest.isEmpty )
-    forest match {
-      case Nil() => Cons(tree,Nil())
-      case Cons(h,t) => 
-        if( tree.rank < h.rank ) 
-          Cons(tree,forest)
-        else
-          insertTree(tree.mergeEqual(h),t)
-    }
-  }
+  // def insertTree(tree: BinomialTree, forest: BinomialForest) : BinomialForest = {
+  //   require( (tree.rank <= forest.rank) || forest.isEmpty )
+  //   forest match {
+  //     case Nil() => Cons(tree,Nil())
+  //     case Cons(h,t) => 
+  //       if( tree.rank < h.rank ) 
+  //         Cons(tree,forest)
+  //       else
+  //         insertTree(tree.mergeEqual(h),t)
+  //   }
+  // }
       
-  def merge(aForest: BinomialForest, bForest: BinomialForest) : BinomialForest = {
-    (aForest,bForest) match {
-      case (Nil(), _) => bForest
-      case (_, Nil()) => aForest
-      case (a @ Cons(ah,at) , b @ Cons(bh,bt)) =>
-        if (ah.rank > bh.rank)
-            Cons(ah, merge(b, at))
-        else if (ah.rank < bh.rank )
-            Cons(bh, merge(a, bt))
-        else
-            insertTree( ah.mergeEqual(bh), merge(at,bt))
-    }
-  } 
-  
-  def rankLemma(t: BinomialTree): Boolean = {
-    t.forest match {
-      case Nil() => t.rank == 0
-      case Cons(h,Nil()) => h.rank == 1
-      case Cons(h,t) => h.rank == t.rank +1
-    }
-  }.holds
+  // def merge(aForest: BinomialForest, bForest: BinomialForest) : BinomialForest = {
+  //   (aForest,bForest) match {
+  //     case (Nil(), _) => bForest
+  //     case (_, Nil()) => aForest
+  //     case (a @ Cons(ah,at) , b @ Cons(bh,bt)) =>
+  //       if (ah.rank > bh.rank)
+  //           Cons(ah, merge(b, at))
+  //       else if (ah.rank < bh.rank )
+  //           Cons(bh, merge(a, bt))
+  //       else
+  //           insertTree( ah.mergeEqual(bh), merge(at,bt))
+  //   }
+  // } 
 }
 
 
